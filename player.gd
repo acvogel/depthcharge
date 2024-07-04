528e80f6a1554af808030d2077b14541e367c2f8
Adam Vogel <acvogel@gmail.com>
2024-07-04 14:42:53 -0700

    Initial commit

---
+++ player.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+signal player_hit
+
+@export var speed = 400 # pixels / sec
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(body):
+	hide()
+	player_hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
