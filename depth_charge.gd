d02308b379d3bd02933040467186556c90755ab8
Adam Vogel <acvogel@gmail.com>
2024-07-11 10:10:39 -0700

    Initial depth charge logic and collisions

--- depth_charge.gd
+++ depth_charge.gd
@@ -1,17 +1,22 @@
 extends RigidBody2D
 
+signal depth_charge_hit
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	pass
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
 
-func despawn():
+func _on_body_entered(body):
+	# despawn depth charge (todo explosion)
 	hide()
+	depth_charge_hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)
+	
+	# despawn submarine (todo explosion animation)
+	body.despawn()
