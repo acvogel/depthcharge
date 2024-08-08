50e222efdcaabec369080fab3c689e86ff0160b1
Adam Vogel <acvogel@gmail.com>
2024-08-08 10:48:41 -0700

    Add GAME OVER text and state

--- main.gd
+++ main.gd
@@ -3,22 +3,31 @@ extends Node
 @export var submarine_scene: PackedScene
 var game_timer = 0.0 # sec
 
+enum GameState {INTRO, GAME, GAME_OVER}
+var game_state = GameState.INTRO
+
 func _ready():
 	new_game()
 
 func new_game():
-	$SubmarineTimer.start()
+	$SubmarineTimer.start(0.0)
+	game_state = GameState.GAME
+	$HUD.get_node("GameOverLabel").visible = false
 
 func end_game():
+	game_state = GameState.GAME_OVER
 	get_tree().call_group("submarines", "queue_free")
 	get_tree().call_group("mines", "queue_free")
 	get_tree().call_group("depth_charges", "queue_free")
 	$SubmarineTimer.stop()
+	# smells, add handler code to $HUD itself
+	$HUD.get_node("GameOverLabel").visible = true
 
 func _process(delta):
-	game_timer += delta
-	$HUD.set_game_timer(game_timer)
-	$HUD.set_score($Player.score)
+	if game_state == GameState.GAME:
+		game_timer += delta
+		$HUD.set_game_timer(game_timer)
+		$HUD.set_score($Player.score)
 
 func _on_submarine_timer_timeout():
 	var submarine = submarine_scene.instantiate()
